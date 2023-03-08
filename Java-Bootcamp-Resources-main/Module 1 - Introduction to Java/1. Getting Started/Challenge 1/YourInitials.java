747be3c12502d6cc95fe8895d6c7c719f94c7584
Fatema Zahra <fatema.zahra1312@gmail.com>
2023-03-08 18:39:38 +0000

    First Challenge

---
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
@@ -0,0 +1,13 @@
+public class YourInitials {
+    public static void main(String[] args) {
+
+        System.out.println("FFFFF ZZZZZ");
+        System.out.println("F         Z");
+        System.out.println("F        Z ");
+        System.out.println("FFF     Z  ");
+        System.out.println("F      Z   ");
+        System.out.println("F     Z    ");
+        System.out.println("F     ZZZZZ");
+         // Instructions for this workbook are on Learn the Part (See the Udemy Video: Your Initials to access the link).
+    }
+}
